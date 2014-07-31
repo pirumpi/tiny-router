d0f06c0b9dcf3a464aa4ec1322ff4e090410b03b
Frijol <ifoundthemeaningoflife@gmail.com>
2014-07-31 14:02:34 -0700

    added examples from readme to examples folder

---
+++ examples/tessel-lights.js
@@ -0,0 +1,29 @@
+var router = require('tiny-router'),
+    tessel = require('tessel');
+
+var lights = { 
+    green: tessel.led[0], 
+    blue: tessel.led[1], 
+    red: tessel.led[2]
+    amber: tessel.led[3]
+};
+
+router
+    .get('/', function(req, res) {
+        res.send('Simple light web API');
+    })
+    .get('/lights', function(req, res){
+        res.send(lights);
+    })
+    .get('/green', function(req, res){
+        var state = lights.green.read();
+        lights.green.write(state);
+        res.send({status: state});
+    })
+    .get('/green/{state}', function(req, res){
+        var state = parseInt(req.body.state);
+        lights.green.write(state);
+        res.send({status: state});
+    });
+
+router.listen(8080);
